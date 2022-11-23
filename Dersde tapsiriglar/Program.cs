8dcedc2d0667cf4df54a465ee1090d667c03978b
Arif244 <117301795+Arif244@users.noreply.github.com>
2022-11-23 18:09:38 +0400

    new solition2

---
+++ Dersde tapsiriglar/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Dersde_tapsiriglar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string test = "salam p327";
+            Console.WriteLine(test.ToUpper());
+        }
+
+
+    }
+
+    static void test(params string[] test)
+    {
+        foreach (string item in test)
+        {
+            Console.WriteLine(test);
+        }
+
+
+
+    }
+}
