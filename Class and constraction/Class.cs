836b0528e82589a1dffb280e29a2428a5a98fbd5
Arif244 <117301795+Arif244@users.noreply.github.com>
2022-11-30 15:42:35 +0400

    gecikmis ev tepsirigi

---
+++ Class and constraction/Class.cs
@@ -0,0 +1,55 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_constraction
+{
+    internal class Student
+    {
+        public string FirstName;
+        public string Surname;
+        public byte Point;
+        public bool IsGraduated;
+
+
+
+
+
+        public Student(string name, string surname,byte point,bool Isgraduated):this(point,Isgraduated)
+        {
+            FirstName = name;
+            Surname = surname;
+            
+        }
+
+        public Student(byte point,bool Isgraduated):this(Isgraduated)
+        {
+            Point = point;
+            if (point>80)
+            {
+                Console.WriteLine("imtahana buraxilir");
+            }
+            else
+            {
+                Console.WriteLine("imtahana buraxilmir");
+            }
+            
+        }
+
+        public Student(bool IsGraduated)
+        {
+            IsGraduated = true;
+
+            if (true)
+            {
+                Console.WriteLine("bu telebe mezun olub");
+            }
+            else 
+            {
+                Console.WriteLine("bu telebe tehsilini davam etdirir");
+            }
+        }
+    }
+}
---
+++ Class and constraction/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Class_and_constraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Arif", "Kerimli", 75,true);
+            Student student1 = new Student("Aga", "Amirov", 95,false);
+
+
+            Console.WriteLine(student1.Point);
+
+
+
+
+
+        }
+
+    }
+}
